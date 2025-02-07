65875c1fff077c80dccf0eae74423fa9bcf74e90
Julakanti ruthwik Reddy <julakantiruthwikreddy2003@gmail.com>
2025-02-08 01:01:35 +0530

    Added search bar and button

--- src/Components/NavBar/Navbar.jsx
+++ src/Components/NavBar/Navbar.jsx
@@ -1,78 +1,72 @@
-// import { useContext } from "react";
-// import { CurrencyContext } from "../../Context/CurrencyContext";
 import { useNavigate } from "react-router-dom";
-import  currencystore from "../../Zustandstate/Store";
-function NavBar(){
-  const {setCurrency} = currencystore();
+import currencystore from "../../Zustandstate/Store";
+import { useState } from "react";
+
+function NavBar() {
+  const { setCurrency } = currencystore();
   const navigate = useNavigate();
-  function goToHome(){
-   navigate('/');
+  const [searchTerm, setSearchTerm] = useState("");
+
+  function goToHome() {
+    navigate("/");
   }
-  return(
+
+  function handleSearchBtnclick(event) {
+    const id = searchTerm.toLowerCase();
+    navigate(`/details/${id}`);
+  }
+
+  return (
     <>
-        <div className="navbar bg-base-100">
-  <div className="navbar-start">
-    <div className="dropdown">
-      <div tabIndex={0} role="button" className="btn btn-ghost btn-circle">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className="w-5 h-5"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor">
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth="2"
-            d="M4 6h16M4 12h16M4 18h7" />
-        </svg>
+      <div className="navbar bg-base-100">
+        <div className="navbar-start">
+          <div className="dropdown">
+            <div tabIndex={0} role="button" className="btn btn-ghost btn-circle">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="w-5 h-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h16M4 18h7"
+                />
+              </svg>
+            </div>
+            <ul
+              tabIndex={0}
+              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
+            >
+              <li onClick={() => setCurrency("inr")}>
+                <a>INR</a>
+              </li>
+              <li onClick={() => setCurrency("usd")}>
+                <a>USD</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div onClick={goToHome} className="navbar-center">
+          <a className="text-xl btn btn-ghost">Crypto Tracker</a>
+        </div>
+        <div className="flex items-center navbar-end">
+          <input
+            type="text"
+            placeholder="Search crypto currency"
+            className="w-40 mr-3 input input-bordered sm:w-64"
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+            onKeyDown={(e) => e.key === "Enter" && handleSearchBtnclick()} // Trigger search on Enter key
+          />
+          <button className="btn btn-info btn-sm" onClick={handleSearchBtnclick}>Search</button>
+        </div>
       </div>
-      <ul
-        tabIndex={0}
-        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
-        <li onClick={() => setCurrency('inr')}><a>INR</a></li>
-        <li onClick={() => setCurrency('usd')}><a>USD</a></li>
-      </ul>
-    </div>
-  </div>
-  <div onClick={goToHome} className="navbar-center">
-    <a className="text-xl btn btn-ghost">Crypto Tracker</a>
-  </div>
-  <div className="navbar-end">
-    <button className="btn btn-ghost btn-circle">
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        className="w-5 h-5"
-        fill="none"
-        viewBox="0 0 24 24"
-        stroke="currentColor">
-        <path
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          strokeWidth="2"
-          d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
-      </svg>
-    </button>
-    <button className="btn btn-ghost btn-circle">
-      <div className="indicator">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className="w-5 h-5"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor">
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth="2"
-            d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9" />
-        </svg>
-        <span className="badge badge-xs badge-primary indicator-item"></span>
-      </div>
-    </button>
-  </div>
-</div>
     </>
   );
 }
+
 export default NavBar;
